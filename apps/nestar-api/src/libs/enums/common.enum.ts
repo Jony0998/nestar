5325b482dfa7e4b19fc0a14e70868333f0052639
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-09-17 00:04:01 +0900

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,21 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = "Something went wrong!",
+  NO_DATA_FOUND = "No data is found!",
+  CREATE_FAILED = "Create is failed!",
+  UPDATE_FAILED = "Update is  failed",
+  UPLOAD_FAILED = 'Upload failed',
+  BAD_REQUEST = 'Bad Request',
+  REMOVE_FAILED = 'Remove failed',
+
+  BLOCKED_USER = "You have been blocked! Please contact The Restaurant",
+  NO_MEMBER_NICK = "No Member with that member Nick!",
+  USED_NICK_PHONE = "You are inserting already used nick or phone!",
+  TOKEN_CREATION_FAILED = "Token creation error!",
+  WRONG_PASSWORD = "Wrong password!",
+  NOT_AUTHENTICATED = "You are not authenticated , Please login first",
+  TOKEN_NOT_EXIST = 'Bearer Token is not provided!',
+  ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles',
+  NOT_ALLOWED_REQUEST = 'Not Allowed Request',
+  PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg or png images!',
+  SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied!',
+}
