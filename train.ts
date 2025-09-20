e9d83a4b5f4ee0cfb9123ca8e8fc19e7a093958e
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-09-20 21:44:34 +0900

    feat: task(ZM) completed

--- train.ts
+++ train.ts
@@ -1,26 +1,48 @@
+// (JONY)
+
+
+
+// TASK ZM:
+
+function reverseInteger(son: number): number {
+  const result = son.toString().split('').reverse().join('');
+  return parseInt(result);
+}
+ 
+console.log(reverseInteger(123456789)); 
+
+
+// Shunday function yozing, va bu function parametr
+// sifatida raqamlarni qabul qilsin. Bu function qabul qilingan
+// raqamlarni orqasiga o'girib qaytarsin
+
+// MASALAN: reverseInteger(123456789); return 987654321;
+
+// Yuqoridagi misolda, function kiritilgan raqamlarni orqasiga
+// o'girib (reverse) qilib qaytarmoqda.
+
+
+
 
 // TASK ZK:
 
+// function printNumbers(): void {
+//   let num: number = 1;
 
-function printNumbers(): void {
-  let num: number = 1;
+//   function next() {
+//     console.log(num);
+//     if (num < 5) {
+//       num++;
+//       setTimeout(next, 1000);
+//     }
+//   }
 
-  function next() {
-    console.log(num);
-    if (num < 5) {
-      num++;
-      setTimeout(next, 1000);
-    }
-  }
-
-  next();  
+//   next();  
  
-}  
+// }  
 
 
-printNumbers();  
-
-
+// printNumbers();  
 
 // Shunday function yozing, bu function har bir soniyada bir marotaba
 // console'ga 1'dan 5'gacha bo'lgan raqamlarni chop etsin va
