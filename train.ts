4c918afe556b90bcbba392bad8d82d7a43e87b12
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-09-26 18:44:52 +0900

    feat: task(ZN) completed

--- train.ts
+++ train.ts
@@ -1,15 +1,36 @@
 // (JONY)
 
+// TASK ZN:
+
+
+function rotateArray<T>(arr: T[], index: number): T[] {
+  const cut = arr.splice(index); 
+  arr.unshift(...cut);
+  return arr;
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5, 6, 8, 9], 6)); 
+
+
+
+// Shunday function yozing, uni array va number parametri bo'lsin.
+// Function'ning vazifasi ikkinchi parametr'da berilgan raqam, birinchi
+// array parametr'ning indeksi bo'yicha hisoblanib, shu indeksgacha bo'lgan
+// raqamlarni indeksdan tashqarida bo'lgan raqamlar bilan o'rnini
+// almashtirib qaytarsin.
+
+// MASALAN: rotateArray([1, 2, 3, 4, 5, 6], 3); return [5, 6, 1, 2, 3, 4];
+
 
 
 // TASK ZM:
 
-function reverseInteger(son: number): number {
-  const result = son.toString().split('').reverse().join('');
-  return parseInt(result);
-}
+// function reverseInteger(son: number): number {
+//   const result = son.toString().split('').reverse().join('');
+//   return parseInt(result);
+// }
  
-console.log(reverseInteger(123456789)); 
+// console.log(reverseInteger(123456789)); 
 
 
 // Shunday function yozing, va bu function parametr
