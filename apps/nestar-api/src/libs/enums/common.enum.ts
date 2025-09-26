ee3ec2cfffd3f49447464553b11df3a3336626cb
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-09-26 15:27:06 +0900

    feat: develop global GraphQL error handling  system

--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -6,12 +6,13 @@ export enum Message {
   UPLOAD_FAILED = 'Upload failed',
   BAD_REQUEST = 'Bad Request',
   REMOVE_FAILED = 'Remove failed',
-
+ 
+  USED_MEMBER_NICK_OR_PHONE = "Already used member nick or phone!",
   BLOCKED_USER = "You have been blocked! Please contact The Restaurant",
   NO_MEMBER_NICK = "No Member with that member Nick!",
   USED_NICK_PHONE = "You are inserting already used nick or phone!",
   TOKEN_CREATION_FAILED = "Token creation error!",
-  WRONG_PASSWORD = "Wrong password!",
+  WRONG_PASSWORD = "Wrong password try again!",
   NOT_AUTHENTICATED = "You are not authenticated , Please login first",
   TOKEN_NOT_EXIST = 'Bearer Token is not provided!',
   ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles',
