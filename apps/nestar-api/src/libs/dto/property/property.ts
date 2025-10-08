f638d910e240cde581f9bae6527114a2e85e4bbb
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-10-08 14:36:55 +0900

    feat: develop getProperty business logic

--- apps/nestar-api/src/libs/dto/property/property.ts
+++ apps/nestar-api/src/libs/dto/property/property.ts
@@ -1,6 +1,7 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { ObjectId } from 'mongoose';
 import { PropertyLocation, PropertyStatus, PropertyType } from '../../enums/property.enum';
+import { Member } from '../member/member';
 
 @ObjectType() // serverdan clentga ketyptganda dtp ni quradigan decorator
 export class Property {
@@ -77,4 +78,8 @@ export class Property {
   @Field(() => Date)
   updatedAt: Date;
 
+  /** from aggregation **/
+
+  @Field(() => Member, {nullable: true})
+  memberDate?: Member;
 }
